2a961e88676e1989990db9a64b530cc2890b47c6
Bstat Mega 2 <justin.bem@gmail.com>
2026-06-07 22:10:17 +0100

    Version Qmd par Jour

---
+++ imports.py
@@ -0,0 +1,13 @@
+# ------------PACKAGES----------
+
+import pandas as pd
+import numpy  as np
+from sqlalchemy import create_engine, text
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
+import warnings
+warnings.filterwarnings("ignore")
--- main.R
+++ main.R
@@ -1,1 +1,21 @@
+#---------PACKAGES ---------
+library(tidyverse)
+library(openxlsx2)
+library(DBI)
+library(ROracle)
+library(RPostgres)
+library(RSQLite)
+library(reticulate)
+library(kableExtra)
+library(scales)
+library(patchwork) 
+library(vars)
+library(broom)
+library(reactable)
 library(quarto)
+
+#-----RETICULATE --------
+use_virtualenv("~/.radian")
+source_python("imports.py")
+
+
