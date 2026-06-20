99398a8054a80942b612a0785cf026765ce976f5
Bstat Mega 2 <justin.bem@gmail.com>
2026-06-20 07:35:45 +0100

    Ajout des fichiers xlsx des donneees

--- main.R
+++ main.R
@@ -24,11 +24,39 @@ source_python("imports.py")
 Sys.setlocale("LC_CTYPE","fr_FR.UTF-8")
 Sys.setlocale("LC_ALL","fr_FR.utf8")
 
-if(!file.exists('stats.xlsx')){
+if(!file.exists('banque.xlsx')){
     # pg_ctl -D /var/lib/postgres/data -l /var/lib/postgres/logfile start
     pgcon<-dbConnect(Postgres(), dbname = 'cerber', host = 'localhost', user = 'dbadmin', 
              password = '12345', port = 5432, timezone = NULL, bigint='integer')
 
-    stats<-"select * from public.bancarisation"|>dbGetQuery(pgcon, statement = _)
-    write_xlsx(stats,"stats.xlsx")
+    capital<-"select id_banque % 10000 as banque, (id_banque -id_banque % 10000)/10000 as id_pays, extract(YEAR from date_valo) as annee, 
+    extract(MONTH from date_valo) as mois, trim(id_poste) as poste, valeur 
+    from public.analyse
+    where id_poste in ('AF088','AF073','X19','X26') and extract(YEAR from date_valo) in (2016,2017,2018)"|>
+    dbGetQuery(pgcon,statement=_)|>pivot_wider(names_from=poste,values_from=valeur)|>
+    rename(bilan=AF088,fpc=AF073,fpr=X19,rwa=X26)
+    write_xlsx(capital,"capital.xlsx")
+
+    
+    loansdeposits<-"select id_banque % 10000 as banque, (id_banque - id_banque % 10000)/10000 as id_pays, extract(YEAR from date_valo) as annee, 
+    extract(MONTH from date_valo) as mois, trim(id_poste) as poste, valeur 
+    from public.analyse
+    where id_poste in ('AF043','AF051','AF057','AF029') and extract(YEAR from date_valo) in (2016,2017,2018)"|>
+    dbGetQuery(pgcon,statement=_)|>pivot_wider(names_from=poste,values_from=valeur)|>
+    rename(credits=AF043, provisions=AF051,npl=AF057,depots=AF029)
+    write_xlsx(loansdeposits,"loansdeposits.xlsx")
+
+
+    liquidity<-"select id_banque % 10000 as banque, (id_banque -id_banque % 10000)/10000 as id_pays, extract(YEAR from date_valo) as annee, 
+    extract(MONTH from date_valo) as mois, trim(id_poste) as poste, valeur 
+    from public.analyse 
+    where id_poste in ('AF028','AF053','AF071','AF084') and extract(YEAR from date_valo) in (2016,2017,2018)"|>
+    dbGetQuery(pgcon,statement=_)|>pivot_wider(names_from=poste,values_from=valeur)|>
+    rename(pnl1=AF028, pnl2=AF053,pnl3=AF071,pnl4=AF084)
+    write_xlsx(liquidity,"liquidity.xlsx")
+
+
+    bks<-"select id_banque % 10000 as banque, (id_banque - id_banque % 10000)/10000 as id_pays, categorie, trim(pays) as pays
+    from public.banque"|>dbGetQuery(pgcon, statement = _)
+    write_xlsx(bks,"banque.xlsx")
 }
